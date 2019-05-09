cd7f196f1729018ec59012cae1f7c19c06ef5cae
Cbain10 <50216002+Cbain10@users.noreply.github.com>
2019-05-09 10:08:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+setTimeout("document.f1.ta1.value = 'UEFA'", 1000)
+setTimeout("document.f1.ta1.value = ''", 1300)
+setTimeout("document.f1.ta2.value = 'Champions'", 1600)
+setTimeout("document.f1.ta2.value = ''", 1900)
+setTimeout("document.f1.ta3.value = 'League!!!'", 2200)
+setTimeout("document.f1.ta3.value = ''", 2500)
+setTimeout("document.f3.ta4.value = 'UEFA'", 2800)
+setTimeout("document.f3.ta4.value = ''", 3100)
+setTimeout("document.f3.ta5.value = 'Champions'", 3400)
+setTimeout("document.f3.ta5.value = ''", 3700)
+setTimeout("document.f3.ta6.value = 'League!!!'", 4000)
+setTimeout("document.f3.ta6.value = ''", 4300)
